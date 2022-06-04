f23ffb17349d42a98a077d0e302dee33110290c4
Izaz <izazul2016s@gmail.com>
2022-06-05 04:06:08 +0600

    firebase

---
+++ lib/Reg_Frome/Home.dart
@@ -0,0 +1,45 @@
+
+import 'package:flutter/material.dart';
+
+import 'Log_In.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Welcome "),
+        centerTitle: true,
+      ),
+
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 250,
+              child: Image.asset("assets/icon.jpg"),
+            ),
+            Text("Welcome Back",style: TextStyle(
+                fontSize: 30
+            ),),
+            Text("Name",style: TextStyle(
+              fontSize: 20,
+            ),),
+            Text("Email",style: TextStyle(
+                fontSize: 30
+            ),),
+            SizedBox(height: 10,),
+            ActionChip(label: Text("Logout",style: TextStyle(
+                fontSize: 30,
+                color: Colors.redAccent
+            ),), onPressed: (){
+              Navigator.push(context, MaterialPageRoute(builder: (context) => MyLogInFrom()));
+            })
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/Reg_Frome/Log_In.dart
@@ -0,0 +1,141 @@
+import 'dart:ffi';
+import 'package:flutter/material.dart';
+
+import 'Home.dart';
+import 'Reg_frome.dart';
+
+
+
+class MyLogInFrom extends StatefulWidget {
+  const MyLogInFrom({Key? key}) : super(key: key);
+
+  @override
+  State<MyLogInFrom> createState() => _MyLogInFromState();
+}
+
+class _MyLogInFromState extends State<MyLogInFrom> {
+  //form key
+  final _formKey = GlobalKey<FormState>();
+
+  final TextEditingController emailController = new TextEditingController();
+  final TextEditingController passwordController = new TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    //email field
+
+    final emailField = TextFormField(
+      autofocus: false,
+      controller: emailController,
+      keyboardType: TextInputType.emailAddress,
+
+      //validator
+      onSaved: (value) {
+        emailController.text = value!;
+      },
+      textInputAction: TextInputAction.next,
+      decoration: InputDecoration(
+          prefixIcon: Icon(Icons.mail),
+          contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+          hintText: "Email",
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(10))),
+    );
+
+    final passowrdField = TextFormField(
+      autofocus: false,
+      controller: passwordController,
+      obscureText: true,
+
+      //validator
+      onSaved: (value) {
+        passwordController.text = value!;
+      },
+      textInputAction: TextInputAction.done,
+      decoration: InputDecoration(
+          prefixIcon: Icon(Icons.vpn_key),
+          suffixIcon: Icon(Icons.remove_red_eye),
+          contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+          hintText: "Password",
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(10))),
+    );
+
+    // bottun
+    final loginButton = Material(
+      elevation: 3,
+      borderRadius: BorderRadius.circular(30),
+      color: Colors.redAccent,
+      child: MaterialButton(
+
+        padding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+        minWidth: MediaQuery.of(context).size.width/2,
+        onPressed: (){
+          Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => MyHomePage()));
+        },
+        child: Text("Login",style: TextStyle(
+            fontSize: 20
+        ),),
+
+      ),
+    );
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: SingleChildScrollView(
+          child: Container(
+            color: Colors.white,
+            padding: EdgeInsets.all(36),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+
+                children: <Widget>[
+                  SizedBox(
+                    height: 150,
+                    child: Image.asset("assets/user.png",
+                      fit: BoxFit.contain,
+                    ),
+
+                  ),
+                  SizedBox(height: 50,),
+                  emailField,
+                  SizedBox(
+                    height: 40,
+                  ),
+                  passowrdField,
+                  SizedBox(height: 40,),
+                  loginButton,
+                  SizedBox(height: 40,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children:<Widget> [
+                      Text("Don't have an account?",
+                        style: TextStyle(fontSize: 15,
+                            color: Colors.black),),
+                      GestureDetector(
+
+                        onTap: (){
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => MyReg_Form()));
+                        },
+
+                        child: Text("SignUp",style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          color: Colors.redAccent,
+                          fontSize: 20,
+                        ),),
+                      )
+                    ],
+                  )
+                ],
+
+              ),
+
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
